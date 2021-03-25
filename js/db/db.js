0bf3429b3e44a0bdc353b59ca6f2ab6841a4c4a8
Myke <45573852+MykeArtDesign@users.noreply.github.com>
2021-03-26 00:05:41 +0100

    display weapons on map

---
+++ js/db/db.js
@@ -0,0 +1,349 @@
+export const weaponsUrl = [
+  {
+    id: 0,
+    name:"bat",
+    url: "assets/img/weapons/bat.png"
+  },
+  {
+    id: 1,
+    name:"firethrower",
+    url: "assets/img/weapons/firethrower.png"
+  },
+  {
+    id: 2,
+    name:"gun",
+    url: "assets/img/weapons/gun.png"
+  },
+  {
+    id: 3,
+    name:"knife",
+    url: "assets/img/weapons/knife.png"
+  },
+  {
+    id: 4,
+    name:"firethrower",
+    url: "assets/img/weapons/riffle.png"
+  }
+];
+
+export const playerWalk = {
+  man: {
+      bat: [
+        {
+          id: 0,
+          url: "assets/img/players/man/walk/bat/Walk_bat_000"
+        },
+        {
+          id: 1,
+          url: "assets/img/players/man/walk/bat/Walk_bat_001"
+        },
+        {
+          id: 2,
+          url: "assets/img/players/man/walk/bat/Walk_bat_002"
+        },
+        {
+          id: 3,
+          url: "assets/img/players/man/walk/bat/Walk_bat_003"
+        },
+        {
+          id: 4,
+          url: "assets/img/players/man/walk/bat/Walk_bat_004"
+        },
+        {
+          id: 5,
+          url: "assets/img/players/man/walk/bat/Walk_bat_005"
+        },
+      ],
+      firethrower: [
+        {
+          id: 0,
+          url: "assets/img/players/man/walk/firethrower/Walk_firethrower_000"
+        },
+        {
+          id: 1,
+          url: "assets/img/players/man/walk/firethrower/Walk_firethrower_001"
+        },
+        {
+          id: 2,
+          url: "assets/img/players/man/walk/firethrower/Walk_firethrower_002"
+        },
+        {
+          id: 3,
+          url: "assets/img/players/man/walk/firethrower/Walk_firethrower_003"
+        },
+        {
+          id: 4,
+          url: "assets/img/players/man/walk/firethrower/Walk_firethrower_004"
+        },
+        {
+          id: 5,
+          url: "assets/img/players/man/walk/firethrower/Walk_firethrower_005"
+        },
+      ],
+      gun: [
+        {
+          id: 0,
+          url: "assets/img/players/man/walk/gun/Walk_gun_000"
+        },
+        {
+          id: 1,
+          url: "assets/img/players/man/walk/gun/Walk_gun_001"
+        },
+        {
+          id: 2,
+          url: "assets/img/players/man/walk/gun/Walk_gun_002"
+        },
+        {
+          id: 3,
+          url: "assets/img/players/man/walk/gun/Walk_gun_003"
+        },
+        {
+          id: 4,
+          url: "assets/img/players/man/walk/gun/Walk_gun_004"
+        },
+        {
+          id: 5,
+          url: "assets/img/players/man/walk/gun/Walk_gun_005"
+        },
+      ],
+      knife: [
+        {
+          id: 0,
+          url: "assets/img/players/man/walk/knife/Walk_knife_000"
+        },
+        {
+          id: 1,
+          url: "assets/img/players/man/walk/knife/Walk_knife_001"
+        },
+        {
+          id: 2,
+          url: "assets/img/players/man/walk/knife/Walk_knife_002"
+        },
+        {
+          id: 3,
+          url: "assets/img/players/man/walk/knife/Walk_knife_003"
+        },
+        {
+          id: 4,
+          url: "assets/img/players/man/walk/knife/Walk_knife_004"
+        },
+        {
+          id: 5,
+          url: "assets/img/players/man/walk/knife/Walk_knife_005"
+        },
+      ],
+      riffle: [
+        {
+          id: 0,
+          url: "assets/img/players/man/walk/riffle/Walk_riffle_000"
+        },
+        {
+          id: 1,
+          url: "assets/img/players/man/walk/riffle/Walk_riffle_001"
+        },
+        {
+          id: 2,
+          url: "assets/img/players/man/walk/riffle/Walk_riffle_002"
+        },
+        {
+          id: 3,
+          url: "assets/img/players/man/walk/riffle/Walk_riffle_003"
+        },
+        {
+          id: 4,
+          url: "assets/img/players/man/walk/riffle/Walk_riffle_004"
+        },
+        {
+          id: 5,
+          url: "assets/img/players/man/walk/riffle/Walk_riffle_005"
+        },
+      ],
+  }
+}
+
+export const playerHit = {
+  man: {
+    bat : [
+      {
+        id: 0,
+        url: "assets/img/players/man/hit/bat/Bat_000"
+      },
+      {
+        id: 1,
+        url: "assets/img/players/man/hit/bat/Bat_001"
+      },
+      {
+        id: 2,
+        url: "assets/img/players/man/hit/bat/Bat_002"
+      },
+      {
+        id: 3,
+        url: "assets/img/players/man/hit/bat/Bat_003"
+      },
+      {
+        id: 4,
+        url: "assets/img/players/man/hit/bat/Bat_004"
+      },
+      {
+        id: 5,
+        url: "assets/img/players/man/hit/bat/Bat_005"
+      },
+      {
+        id: 6,
+        url: "assets/img/players/man/hit/bat/Bat_006"
+      },
+      {
+        id: 7,
+        url: "assets/img/players/man/hit/bat/Bat_007"
+      },
+      {
+        id: 8,
+        url: "assets/img/players/man/hit/bat/Bat_008"
+      },
+      {
+        id: 9,
+        url: "assets/img/players/man/hit/bat/Bat_009"
+      },
+      {
+        id: 10,
+        url: "assets/img/players/man/hit/bat/Bat_010"
+      },
+      {
+        id: 11,
+        url: "assets/img/players/man/hit/bat/Bat_011"
+      },
+    ],
+    firethrower : [
+      {
+        id: 0,
+        url: "assets/img/players/man/hit/firethrower/FlameThrower_000"
+      },
+      {
+        id: 1,
+        url: "assets/img/players/man/hit/firethrower/FlameThrower_001"
+      },
+      {
+        id: 2,
+        url: "assets/img/players/man/hit/firethrower/FlameThrower_002"
+      },
+      {
+        id: 3,
+        url: "assets/img/players/man/hit/firethrower/FlameThrower_003"
+      },
+      {
+        id: 4,
+        url: "assets/img/players/man/hit/firethrower/FlameThrower_004"
+      },
+      {
+        id: 5,
+        url: "assets/img/players/man/hit/firethrower/FlameThrower_005"
+      },
+      {
+        id: 6,
+        url: "assets/img/players/man/hit/firethrower/FlameThrower_006"
+      },
+      {
+        id: 7,
+        url: "assets/img/players/man/hit/firethrower/FlameThrower_007"
+      },
+      {
+        id: 8,
+        url: "assets/img/players/man/hit/firethrower/FlameThrower_008"
+      }
+    ],
+    gun : [
+      {
+        id: 0,
+        url: "assets/img/players/man/hit/gun/Gun_Shot_000"
+      },
+      {
+        id: 1,
+        url: "assets/img/players/man/hit/gun/Gun_Shot_001"
+      },
+      {
+        id: 2,
+        url: "assets/img/players/man/hit/gun/Gun_Shot_002"
+      },
+      {
+        id: 3,
+        url: "assets/img/players/man/hit/gun/Gun_Shot_003"
+      },
+      {
+        id: 4,
+        url: "assets/img/players/man/hit/gun/Gun_Shot_004"
+      }
+    ],
+    knife : [
+      {
+        id: 0,
+        url: "assets/img/players/man/hit/knife/Knife_000"
+      },
+      {
+        id: 1,
+        url: "assets/img/players/man/hit/knife/Knife_001"
+      },
+      {
+        id: 2,
+        url: "assets/img/players/man/hit/knife/Knife_002"
+      },
+      {
+        id: 3,
+        url: "assets/img/players/man/hit/knife/Knife_003"
+      },
+      {
+        id: 4,
+        url: "assets/img/players/man/hit/knife/Knife_004"
+      },
+      {
+        id: 5,
+        url: "assets/img/players/man/hit/knife/Knife_005"
+      },
+      {
+        id: 6,
+        url: "assets/img/players/man/hit/knife/Knife_006"
+      },
+      {
+        id: 7,
+        url: "assets/img/players/man/hit/knife/Knife_007"
+      }
+    ],
+    riffle : [
+      {
+        id: 0,
+        url: "assets/img/players/man/hit/riffle/Riffle_000"
+      },
+      {
+        id: 1,
+        url: "assets/img/players/man/hit/riffle/Riffle_001"
+      },
+      {
+        id: 2,
+        url: "assets/img/players/man/hit/riffle/Riffle_002"
+      },
+      {
+        id: 3,
+        url: "assets/img/players/man/hit/riffle/Riffle_003"
+      },
+      {
+        id: 4,
+        url: "assets/img/players/man/hit/riffle/Riffle_004"
+      },
+      {
+        id: 5,
+        url: "assets/img/players/man/hit/riffle/Riffle_005"
+      },
+      {
+        id: 6,
+        url: "assets/img/players/man/hit/riffle/Riffle_006"
+      },
+      {
+        id: 7,
+        url: "assets/img/players/man/hit/riffle/Riffle_007"
+      },
+      {
+        id: 8,
+        url: "assets/img/players/man/hit/riffle/Riffle_008"
+      }
+    ],
+  }
+}
--- js/level-design.js
+++ js/level-design.js
@@ -1,3 +1,5 @@
+import { weaponsUrl } from "./db/db.js";
+
 class LevelDesign {
   constructor() {
     this.map = [];
@@ -6,7 +8,6 @@ class LevelDesign {
       gates: ["gate1", "gate2", "gate3"]
     }
     this.generateMap();
-    this.mapDesign();
   }
   
   generateMap() {
@@ -17,8 +18,8 @@ class LevelDesign {
         let mapping = Math.floor(Math.random() * 2);
         if (mapping === 1) count++;
         if (count > 3
-          || (line[j-1] === 1 && line[j-2] === 1)
-          || ( i !== 0 && this.map[i-1][j]=== 1) ) {
+          || (line[j - 1] === 1 && line[j - 2] === 1)
+          || (i !== 0 && this.map[i - 1][j] === 1)) {
           mapping = 0;
         }
         line = [...line, mapping];
@@ -26,13 +27,16 @@ class LevelDesign {
       this.map.push(line);
     }
     console.log(this.map && "it's work!");
+    this.generateWeapons();
+    this.mapDesign();
   }
   
   mapDesign() {
+    let id = 0;
     const mapContainer = $("#level-design");
     if (this.map) {
       for (let i = 0; i < this.map.length; i++) {
-        mapContainer.append(`<div id="row${i+1}" class="row"></div>`)
+        mapContainer.append(`<div id="row${i + 1}" class="row"></div>`)
         for (let j = 0; j < this.map[i].length; j++) {
           const mapRow = $(`#row${i}`);
           const mapValue = this.map[i][j];
@@ -42,8 +46,24 @@ class LevelDesign {
           const gateClass = this.levelDesign.gates[randomGate];
           if (mapValue === 0) {
             mapRow.append(`<div class='case ${wayClass}'></div>`)
+          } else if (mapValue === 2) {
+            if(weaponsUrl[id]){
+              mapRow.append(`<div class='case ${wayClass}'><img class="weapon" src="${weaponsUrl[id].url}" alt="${weaponsUrl[id].name}"/></div>`)
+              id++;
+            }
           } else mapRow.append(`<div class='case ${gateClass}'></div>`)
         }
+      }
+    }
+  }
+  
+  generateWeapons() {
+    for(let i =0; i < weaponsUrl.length; i++) {
+      const weapons = this.randomPosition();
+      if (weapons) {
+      const x = weapons[0];
+      const y = weapons[1];
+      this.map[x][y] = 2;
       }
     }
   }
@@ -56,8 +76,6 @@ class LevelDesign {
       return [x, y];
     } else this.randomPosition();
   }
-  
 }
 
 const map = new LevelDesign();
-map.randomPosition();
